f777199b9f9cc6247a6fc0014f9fd5b11119e119
Jason Flair <emelieobumse100@gmail.com>
2023-04-18 19:03:26 +0100

    till i figure it out

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,19 +2,26 @@
 """implementin basic caching"""
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-  """Basic Cache class"""
-  def __init__(self):
-    """initialiser"""
-    super()
-  
-  def put(self, key, item):
-    """sets keys to item
-    and saves to cache data"""
-    if key is None:
-      pass
-    self.cache_data[key] = item
-    
-  def get(self, key):
-    """gets the item of the key requested"""
-    return self.cache_data[key]
+    """Basic Cache class"""
+
+    def __init__(self):
+        """initialiser"""
+        super().__init__()
+
+    def put(self, key, item):
+        """sets keys to item
+        and saves to cache data"""
+        if key is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """gets the item of the key requested"""
+        try:
+            if key is None:
+                return None
+            return self.cache_data[key]
+        except KeyError:
+            return None
