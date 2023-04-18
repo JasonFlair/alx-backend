2bcdf55a976daab4e34de171bde0d76c85009ed3
Jason Flair <emelieobumse100@gmail.com>
2023-04-18 23:17:14 +0100

    LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
+"""implementin basic caching"""
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """
-    FIFOCache defines a FIFO caching system
-    """
+    """Basic Cache class"""
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
+        """initialiser"""
         super().__init__()
-        self.order = []
 
     def put(self, key, item):
-        """
-        Cache a key-value pair
-        """
+        """sets keys to item
+        and saves to cache data"""
         if key is None or item is None:
             pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[-1]))
-                del self.cache_data[self.order[-1]]
-                del self.order[-1]
-            if key in self.order:
-                del self.order[self.order.index(key)]
-            self.order.append(key)
-            self.cache_data[key] = item
+        try:
+            initial_vacancy_checked = self.cache_data[key]
+        except KeyError:
+            initial_vacancy_checked = None
+        self.cache_data[key] = item
+
+        if len(self.cache_data) == self.MAX_ITEMS:
+            # will only set last_in at replacement or first set of
+            # input where length of cached_data == max_items
+            # store last in
+            self.last_in = key
+        # if cache data has more than max items,
+        # implementation of last in last out if new key was added
+        if len(self.cache_data) > self.\
+                MAX_ITEMS and initial_vacancy_checked is None:
+            print("Discard: {}".format(self.last_in))
+            # delete the key that was the last before
+            # the increase from MAX_ITEMS
+            del self.cache_data[self.last_in]
+        # if key was simply replaced, store it as last in for future reference
+        self.last_in = key
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
+        """gets the item of the key requested"""
+        try:
+            if key is None:
+                return None
             return self.cache_data[key]
-        return None
+        except KeyError:
+            return None
