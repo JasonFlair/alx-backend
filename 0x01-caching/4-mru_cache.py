2b85aa5eff45b4192ac11b5615c1075c85283c7a
Jason Flair <emelieobumse100@gmail.com>
2023-04-19 15:06:53 +0100

    LRU caching system

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,7 +5,7 @@ import datetime
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """Basic Cache class"""
 
     def __init__(self):
@@ -22,6 +22,7 @@ class LRUCache(BaseCaching):
             initial_vacancy_checked = self.cache_data[key]
         except KeyError:
             initial_vacancy_checked = None
+        self.last_in = key
         self.cache_data[key] = item
         self.cache_data_ages[key] = datetime.datetime.now()
         cache_ages = {}
@@ -33,48 +34,46 @@ class LRUCache(BaseCaching):
                 # create a separate dictionary for iteration.
                 cache_ages[k] = v
             ages_list = [age for age in self.cache_data_ages.values()]
-            least_recent_age = ages_list[0]
+            most_recent_age = ages_list[0]
 
             # set least_recent_age
             for i in ages_list:
-                # if the datetime obj is older than i,
-                # least_recent_age is re-set
-                if least_recent_age > i:
-                    least_recent_age = i
+                # if the datetime obj is newer than i, most_recent_age is re-set
+                if most_recent_age < i:
+                    most_recent_age = i
 
             # compare the key that has the age as its own
             for k, v in cache_ages.items():
-                if v == least_recent_age:
-                    least_recent_used = k
+                if v == most_recent_age:
+                    most_recent_used = k
                     # remove the age k, v pair to update it
-                    del self.cache_data_ages[least_recent_used]
+                    del self.cache_data_ages[most_recent_used]
 
-                    print("Discard: {}".format(least_recent_used))
+                    print("Discard: {}".format(most_recent_used))
 
-                    # remove the least recently used
-                    del self.cache_data[least_recent_used]
+                    # remove the most recently used
+                    del self.cache_data[most_recent_used]
         # if key was simply replaced
         if len(self.cache_data) > self. \
                 MAX_ITEMS and initial_vacancy_checked is not None:
             ages_list = [age for age in self.cache_data_ages.values()]
-            least_recent_age = ages_list[0]
+            most_recent_age = ages_list[0]
 
             # set least_recent_age
             for i in ages_list:
-                # if the datetime obj is older than i,
-                # least_recent_age is re-set
-                if least_recent_age > i:
-                    least_recent_age = i
+                # if the datetime obj is older than i, least_recent_age is re-set
+                if most_recent_age < i:
+                    most_recent_age = i
 
             # compare the key that has the age as its own
             for k, v in cache_ages.items():
-                if v == least_recent_age:
-                    least_recent_used = k
+                if v == most_recent_age:
+                    most_recent_used = k
                     # remove the age k, v pair to update it
-                    del self.cache_data_ages[least_recent_used]
+                    del self.cache_data_ages[most_recent_used]
 
                     # remove the least recently used
-                    del self.cache_data[least_recent_used]
+                    del self.cache_data[most_recent_used]
 
     def get(self, key):
         """gets the item of the key requested"""
